b170413f1ab3333662cce30033e0209850f3d3bc
Draouil Rayssen <131086403+rayssen-draouil@users.noreply.github.com>
2026-02-09 20:38:49 +0100

    Revert "Initial commit - menu microservice structure"
    This reverts commit 4be89a8611bf73f4198a33b352c43e73688fe4d8.

--- spring-microservices/menu-microservice/src/main/java/tn/esprit/menu/repository/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.menu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.menu.entity.Menu;
-
-public interface MenuRepository extends JpaRepository<Menu, Long> {
-}
